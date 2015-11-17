d441f3d975906f564a6d652e12b3680ccf2d679b
Nic Schumann <nicolas_schumann@brown.edu>
2015-11-17 16:20:25 -0500

    module invocation based on command line subcommand switchl; argument forwarding

--- Main.js
+++ Main.js
@@ -2,4 +2,23 @@
 
 var args = require('./Config');
 
-console.log( args );
+switch ( args.subcommand ) {
+
+	case "parse":
+		require('./parse/Main')( args );
+		break;
+
+	case "build":
+		require('./build/Main')( args );
+		break;
+
+	case "serve":
+		require('./serve/Main')( args );
+		break;
+
+	default: 
+		console.error("Unrecognized Subcommand");
+		return -1;
+}
+
+return 0;
