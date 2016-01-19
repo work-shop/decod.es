5dbf5564925a299b607631faf459b325f61c29f2
Nic Schumann <nicolas_schumann@brown.edu>
2016-01-19 14:49:40 -0500

    simplified to print output-chart after firebase post conclusion

--- parse/Main.js
+++ parse/Main.js
@@ -3,14 +3,23 @@
 
 var walk = require('./Walker');
 
+
 module.exports = function( args ) {
 
-	walk( args, args.source, function( err, structure ) {
+	walk( args, args.source, function( err, log ) {
 
 		if ( err ) { console.error( err ); }
 
-		
-		
+		else {
+
+			process.stderr.write( log.print() );
+
+			//process.stdout.write( util.inspect( log.json(), false, null) );
+
+			process.exit( 0 );
+
+		}
+
 	});
 
 };
